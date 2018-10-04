568277a8a40920fce638519ce9576ec747731375
Robert Snyder <r.snyder49@gmail.com>
2018-10-04 19:36:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   #"SELECT color FROM bears WHERE color COUNT "
-  "SELECT MAX(color) FROM bears"
+  "SELECT MAX(color) COUNT(color) FROM bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
